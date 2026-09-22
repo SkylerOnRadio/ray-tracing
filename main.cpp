202a66b65a213e08153bb856c6b2f18600aa6d50
SkylerOnRadio <abhigyadulal@gmail.com>
2026-09-22 21:31:18 +0530

    Created camera class for all the work of the camera

--- main.cpp
+++ main.cpp
@@ -1,73 +1,23 @@
+#include "camera.h"
 #include "colour.h"
-#include "hittable.h"
 #include "hittableList.h"
-#include "interval.h"
 #include "ray.h"
 #include "sphere.h"
 #include "utility.h"
 #include "vec3.h"
-#include <iostream>
-
-color ray_color(const ray &r, const hittable &world) {
-  hit_record rec;
-  if (world.hit(r, interval(0, infinity), rec)) {
-    return .5 * (rec.normal + color(1, 1, 1));
-  }
-
-  vec3 unit_dir = unit_vector(r.direction());
-  auto a = .5 * (unit_dir.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(.5, .7, 1.0);
-}
 
 int main() {
-  auto aspect_ratio = 16.0 / 9.0;
-  int img_width = 400;
-
-  int img_height = int(img_width / aspect_ratio);
-  img_height = (img_height < 1) ? 1 : img_height;
-
   hittable_list world;
 
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-  auto focal_length = 1.0;
-  auto viewport_height = 2.0;
-  // we do not use aspect_ratio because it is the ideal aspect ratio, but the
-  // real ratio may not be so, eg if the height is less than 1, we set it to 1
-  // so it is possible for the true aspect ratio to be different from the ideal
-  // one
-  auto viewport_width = viewport_height * (double(img_width) / img_height);
-  auto camera_center = point3(0, 0, 0);
+  camera cam;
 
-  auto viewport_u = vec3(viewport_width, 0, 0);
-  auto viewport_v = vec3(0, -viewport_height, 0);
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
 
-  auto pixel_delta_u = viewport_u / img_width;
-  auto pixel_delta_v = viewport_v / img_height;
-
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                             viewport_u / 2 - viewport_v / 2;
-  auto pixel00_loc = viewport_upper_left + .5 * (pixel_delta_u + pixel_delta_v);
-
-  // printing out the configuration of the image described in the PPM file
-  std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
-
-  for (int j = 0; j < img_height; ++j) {
-    std::clog << "\rScanlines remaining: " << (img_height - j) << ' '
-              << std::flush;
-    for (int i = 0; i < img_width; ++i) {
-      auto pixel_center =
-          pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-      auto ray_direction = pixel_center - camera_center;
-      ray r(camera_center, ray_direction);
-
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
-    }
-  }
-
-  std::clog << "\nDone.\n";
+  cam.render(world);
 
   return 0;
 }
